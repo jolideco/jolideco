a81b215173fe7a6b3d0331a3e9ab775b3dde3da3
Axel Donath <axel.donath@mpi-hd.mpg.de>
2022-06-06 11:40:28 -0400

    Add optional skimage dependecy

--- jolideco/utils/numpy.py
+++ jolideco/utils/numpy.py
@@ -1,5 +1,26 @@
+
+__all__ = ["view_as_overlapping_patches"]
+
+
 def view_as_overlapping_patches(image, shape):
-    """View as overlapping patches"""
+    """View as overlapping patches
+
+    Parameters
+    ----------
+    image : `~numpy.ndarray`
+        Image tensor
+    shape : tuple of int
+        Shape of the patches.
+
+    Returns
+    -------
+    patches : `~numpy.ndarray`
+        Array of overlapping patches of shape
+        (n_patches, patch_shape_flat)
+
+    """
+    from skimage.util import view_as_windows
+
     step = shape[0] // 2
     patches = view_as_windows(image, shape, step=step)
     ncols = shape[0] * shape[1]
