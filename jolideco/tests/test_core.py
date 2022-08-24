8789456a0ae394c1f3210ac7fd4b5d9a4b87a547
Axel Donath <axel.donath@mpi-hd.mpg.de>
2022-08-24 11:14:58 -0400

    Adapt tests

--- jolideco/tests/test_core.py
+++ jolideco/tests/test_core.py
@@ -7,16 +7,16 @@ from jolideco.data import disk_source_gauss_psf, gauss_and_point_sources_gauss_p
 from jolideco.models import FluxComponent, FluxComponents
 from jolideco.priors import Priors, UniformPrior
 
-RANDOM_STATE = np.random.RandomState(642020)
-
 
 @pytest.fixture(scope="session")
 def datasets_gauss():
     datasets = []
 
+    random_state = np.random.RandomState(642020)
+
     for idx in range(3):
         dataset = gauss_and_point_sources_gauss_psf(
-            random_state=RANDOM_STATE,
+            random_state=random_state,
         )
         datasets.append(dataset)
 
@@ -27,9 +27,11 @@ def datasets_gauss():
 def datasets_disk():
     datasets = []
 
+    random_state = np.random.RandomState(642020)
+
     for idx in range(3):
         dataset = disk_source_gauss_psf(
-            random_state=RANDOM_STATE,
+            random_state=random_state,
         )
         datasets.append(dataset)
 
@@ -45,13 +47,13 @@ def deconvolver_result(datasets_gauss):
         n_epochs=100,
         learning_rate=0.1,
         loss_function_prior=priors,
-        upsampling_factor=1,
     )
 
-    fluxes_init = RANDOM_STATE.gamma(20, size=(32, 32))
+    random_state = np.random.RandomState(642020)
+    flux_init = random_state.gamma(20, size=(32, 32))
 
     components = FluxComponents()
-    components["flux-1"] = FluxComponent.from_flux_init_numpy(flux_init=fluxes_init)
+    components["flux-1"] = FluxComponent.from_flux_init_numpy(flux_init=flux_init)
 
     result = deco.run(datasets=datasets_gauss, components=components)
     return result
@@ -63,14 +65,14 @@ def test_map_deconvolver_str():
 
 
 def test_map_deconvolver_result(deconvolver_result):
-    assert_allclose(deconvolver_result.flux_total[12, 12], 1.551957, rtol=1e-3)
-    assert_allclose(deconvolver_result.flux_total[0, 0], 0.204177, rtol=1e-3)
+    assert_allclose(deconvolver_result.flux_total[12, 12], 1.858458, rtol=1e-3)
+    assert_allclose(deconvolver_result.flux_total[0, 0], 0.272629, rtol=1e-3)
 
     trace_loss = deconvolver_result.trace_loss[-1]
-    assert_allclose(trace_loss["total"], 5816.533375, rtol=1e-3)
-    assert_allclose(trace_loss["dataset-0"], 1949.894236, rtol=1e-3)
-    assert_allclose(trace_loss["dataset-1"], 1943.252298, rtol=1e-3)
-    assert_allclose(trace_loss["dataset-2"], 1923.386841, rtol=1e-3)
+    assert_allclose(trace_loss["total"], 5.680209, rtol=1e-3)
+    assert_allclose(trace_loss["dataset-0"], 1.904194, rtol=1e-3)
+    assert_allclose(trace_loss["dataset-1"], 1.897707, rtol=1e-3)
+    assert_allclose(trace_loss["dataset-2"], 1.878308, rtol=1e-3)
 
 
 def test_map_deconvolver_result_io(deconvolver_result, tmpdir):
@@ -80,8 +82,8 @@ def test_map_deconvolver_result_io(deconvolver_result, tmpdir):
     result = MAPDeconvolverResult.read(filename=filename)
 
     assert result.config["n_epochs"] == 100
-    assert_allclose(result.flux_total[12, 12], 1.551957, rtol=1e-3)
-    assert_allclose(result.flux_total[0, 0], 0.204177, rtol=1e-3)
+    assert_allclose(result.flux_total[12, 12], 1.858458, rtol=1e-3)
+    assert_allclose(result.flux_total[0, 0], 0.272629, rtol=1e-3)
 
 
 def test_map_deconvolver_result_plot(deconvolver_result):
@@ -97,10 +99,10 @@ def test_map_deconvolver_usampling(datasets_disk):
         n_epochs=100,
         learning_rate=0.1,
         loss_function_prior=priors,
-        upsampling_factor=2,
     )
 
-    flux_init = RANDOM_STATE.gamma(20, size=(32, 32))
+    random_state = np.random.RandomState(642020)
+    flux_init = random_state.gamma(20, size=(32, 32))
 
     components = FluxComponents()
     components["flux-1"] = FluxComponent.from_flux_init_numpy(
@@ -110,11 +112,11 @@ def test_map_deconvolver_usampling(datasets_disk):
     result = deco.run(datasets=datasets_disk, components=components)
 
     assert result.flux_upsampled_total.shape == (64, 64)
-    assert_allclose(result.flux_total[12, 12], 2.960147, rtol=1e-3)
-    assert_allclose(result.flux_total[0, 0], 0.894996, rtol=1e-3)
+    assert_allclose(result.flux_total[12, 12], 0.238865, rtol=1e-3)
+    assert_allclose(result.flux_total[0, 0], 0.222219, rtol=1e-3)
 
     trace_loss = result.trace_loss[-1]
-    assert_allclose(trace_loss["total"], 5895.711304, rtol=1e-3)
-    assert_allclose(trace_loss["dataset-0"], 1970.38928, rtol=1e-3)
-    assert_allclose(trace_loss["dataset-1"], 1938.160889, rtol=1e-3)
-    assert_allclose(trace_loss["dataset-2"], 1987.161133, rtol=1e-3)
+    assert_allclose(trace_loss["total"], 5.784892, rtol=1e-3)
+    assert_allclose(trace_loss["dataset-0"], 1.919231, rtol=1e-3)
+    assert_allclose(trace_loss["dataset-1"], 1.937949, rtol=1e-3)
+    assert_allclose(trace_loss["dataset-2"], 1.927711, rtol=1e-3)
