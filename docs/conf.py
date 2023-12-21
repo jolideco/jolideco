5dd8bf07cbdb86e80f0bb6ad12516b635b2ae23f
Axel Donath <axel.donath@cfa.harvard.edu>
2023-12-21 17:00:16 -0500

    Add sphinx design

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,7 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx_automodapi.automodapi",
     "sphinx_automodapi.smart_resolver",
+    "sphinx_design",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
